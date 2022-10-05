747478d18eabd667df87471b04af2845c624a096
Shrinmi <Michelle.Rocha@fundacion-jala.org>
2022-10-05 18:36:07 -0400

    Clase 2: Dinosaur

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -11,8 +11,11 @@ class ObstacleManager:
 
     def update(self, game):
         if len(self.obstacles)==0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
-            self.obstacles.append(Cactus(LARGE_CACTUS))
+            n = random.randint(0, 2)
+            if n == 0:
+                self.obstacles.append(Cactus(SMALL_CACTUS))
+            elif n == 1:
+                self.obstacles.append(Cactus(LARGE_CACTUS))
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
