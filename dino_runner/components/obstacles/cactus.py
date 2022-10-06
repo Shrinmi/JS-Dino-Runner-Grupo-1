709f246d9ad625713322b8d21e8e58057a314ea7
Shrinmi <Michelle.Rocha@fundacion-jala.org>
2022-10-05 22:39:28 -0400

    Class 3: Dinosaur

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -1,8 +1,14 @@
 import random
 from .obstacle import Obstacles
 
-class Cactus(Obstacles):
+class SmallCactus(Obstacles):
     def __init__(self, images):
         type = random.randint(0, 2)
         super().__init__(images, type)
         self.rect.y = 325
+
+class LargeCactus(Obstacles):
+    def __init__(self, images):
+        type = random.randint(0, 2)
+        super().__init__(images, type)
+        self.rect.y = 300
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
-from .cactus import Cactus
+from .cactus import SmallCactus, LargeCactus
 
 
 
@@ -9,21 +9,24 @@ class ObstacleManager:
     def __init__(self):
         self.obstacles=[]
 
-    def update(self, game):
+    def update(self, game_speed, player, on_death):
         if len(self.obstacles)==0:
             n = random.randint(0, 2)
             if n == 0:
-                self.obstacles.append(Cactus(SMALL_CACTUS))
+                self.obstacles.append(SmallCactus(SMALL_CACTUS))
             elif n == 1:
-                self.obstacles.append(Cactus(LARGE_CACTUS))
+                self.obstacles.append(LargeCactus(LARGE_CACTUS))
 
         for obstacle in self.obstacles:
-            obstacle.update(game.game_speed, self.obstacles)
-            if game.player.dino_rect.colliderect(obstacle.rect):
+            obstacle.update(game_speed, self.obstacles)
+            if player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
-                game.playing =False
+                on_death()
                 break
 
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+
+    def reset_obstacles(self):
+        self.obstacles=[]
