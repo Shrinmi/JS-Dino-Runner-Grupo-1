f75ae2d13228b88400368db68f71ea25fb380d30
Shrinmi <Michelle.Rocha@fundacion-jala.org>
2022-10-07 01:07:16 -0400

    Class 4: Dinosaur

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,7 +1,10 @@
 import pygame
 from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
-from dino_runner.utils.constants import BG, ICON, RUNNING, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.components.player_hearts.player_heart_manager import PlayerHeartManager
+from dino_runner.components.power_ups.power_up_manager import PowerUpManager
+from dino_runner.components.power_ups.shield import Shield
+from dino_runner.utils.constants import BG, DEFAULT_TYPE, ICON, RUNNING, SCREEN_HEIGHT, SCREEN_WIDTH, SHIELD_TYPE, TITLE, FPS
 from .score import Score
 from dino_runner.utils.constants import FONT_STYLE
 
@@ -21,9 +24,12 @@ class Game:
 
         self.player= Dinosaur()
         self.obstacle_manager = ObstacleManager()
+        self.power_up_manager = PowerUpManager()
+        self.heart_manager = PlayerHeartManager()
 
         self.death_count=0
         self.score = Score()
+        # self.shields = [Shield()]
 
     def execute(self):
         self.executing = True
@@ -33,11 +39,12 @@ class Game:
         pygame.quit()
 
     def run(self):
-        # Game loop: events - update - draw
         self.game_speed = 15
         self.playing = True
         self.obstacle_manager.reset_obstacles()
         self.score.reset_score()
+        self.power_up_manager.reset_power_ups()
+        self.heart_manager.reset_hearts()
         while self.playing:
             self.events()
             self.update()
@@ -53,17 +60,23 @@ class Game:
         self.player.update(user_input)
         self.obstacle_manager.update(self.game_speed, self.player, self.on_death)
         self.score.update(self)
+        # self.shields[0].update(self.game_speed, self.shields)
+        self.power_up_manager.update(self.game_speed, self.player, self.score.score)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
         self.player.draw(self.screen)
-        #self.obstacles[0].draw(self.screen)
         self.obstacle_manager.draw(self.screen)
         self.score.draw(self.screen)
+        # self.shields[0].draw(self.screen)
+        self.power_up_manager.draw(self.screen)
+        self.draw_power_up_active(self.screen)
+        self.heart_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
+
 
     def draw_background(self):
         image_width = BG.get_width()
@@ -78,6 +91,7 @@ class Game:
         self.screen.fill((127,255,212))#pintar mi ventana
         half_screen_height = SCREEN_HEIGHT //2
         half_screen_width = SCREEN_WIDTH //2
+        
         font = pygame.font.SysFont(FONT_STYLE, 30)
         if self.death_count == 0:#mostrar mensaje bienvenida
             text_component = font.render("Press any key to start", True, (20,51,51))
@@ -102,7 +116,28 @@ class Game:
                 self.run()
 
     def on_death(self):
-        self.playing = False
-        self.death_count += 1
-#        print("Mori")
-#        print(f"Numero muertes: {self.death_count}")
+        has_shield = self.player.type == SHIELD_TYPE 
+        is_invencible = has_shield or self.heart_manager.heart_count > 0
+        if not has_shield:
+            self.heart_manager.reduce_heart()
+        
+        if not is_invencible:
+            pygame.time.delay(500)
+            self.playing = False
+            self.death_count += 1
+
+        return is_invencible
+
+    def draw_power_up_active(self, screen):
+        if self.player.has_power_up:
+            time_to_show = round ((self.player.power_up_time_up - pygame.time.get_ticks()) / 1000, 2)
+            if time_to_show >= 0:
+                font = pygame.font.SysFont(FONT_STYLE, 18)
+                text_component = font.render(f"{self.player.type.capitalize()} enabled for {time_to_show} seconds", True, (0,0,0))
+                text_rect = text_component.get_rect()
+                text_rect.center = (500, 40)
+                screen.blit(text_component, text_rect)
+            else:
+                self.player.has_power_up = False
+                self.player.type = DEFAULT_TYPE
+                
